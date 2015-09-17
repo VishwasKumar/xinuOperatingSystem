08567c17197ddf06bec720694402e6f32762bed6
Vishwas <visvijay@indiana.edu>
2015-09-17 09:07:13 -0400

    added conditions with appropriate error message

--- bbb-xinu/shell/xsh_hello.c
+++ bbb-xinu/shell/xsh_hello.c
@@ -13,9 +13,11 @@ shellcmd xsh_hello(int nargs, char *args[]) {
 
 	int32	i;			/* walks through args array	*/
 
-	if (nargs > 2 || nargs < 2)
-		printf("Invalid number of arguments\n");
-	 else
+	if (nargs < 2)
+		printf("Too few arguments\n");
+	else if (nargs > 2)
+		printf("Too many arguments\n");
+	else
 		printf("Hello %s, Welcome to the world of Xinu!!\n", args[1]);
 	return 0;
 }
